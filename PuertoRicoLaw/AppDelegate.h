01c6c9b8c4f40df9b1ab6bed0ebd539ed66a69c9
Axel Rivera <axel@riveralabs.com>
2011-12-20 14:36:16 -0400

    Initial Commit

---
+++ PuertoRicoLaw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PuertoRicoLaw
+//
+//  Created by Axel Rivera on 12/20/11.
+//  Copyright (c) 2011 Axel Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PuertoRicoLaw/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PuertoRicoLaw
+//
+//  Created by Axel Rivera on 12/20/11.
+//  Copyright (c) 2011 Axel Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
